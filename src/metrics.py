e083848ca133b799b4280e6a91b204676a35d8c2
mechanic3000 <70775384+mechanic3000@users.noreply.github.com>
2023-01-06 22:50:38 +0300

    precision_k

--- src/metrics.py
+++ src/metrics.py
@@ -12,18 +12,27 @@ def precision(recommended_list, bought_list):
     return precision
 
 
+# def precision_at_k(recommended_list, bought_list, k=5):
+#     bought_list = np.array(bought_list)
+#     recommended_list = np.array(recommended_list)
+    
+
+#     bought_list = bought_list  # Тут нет [:k] !!
+    
+#     if k < len(recommended_list):
+#         recommended_list = recommended_list[:k]
+
+#     flags = np.isin(bought_list, recommended_list)
+
+#     precision = flags.sum() / len(recommended_list)
+
+#     return precision
+
+
 def precision_at_k(recommended_list, bought_list, k=5):
+    recommended_list = np.array(recommended_list[:k])
     bought_list = np.array(bought_list)
-    recommended_list = np.array(recommended_list)
-    
-
-    bought_list = bought_list  # Тут нет [:k] !!
-    
-    if k < len(recommended_list):
-        recommended_list = recommended_list[:k]
-
-    flags = np.isin(bought_list, recommended_list)
-
+    flags = np.isin(recommended_list, bought_list) * 1
     precision = flags.sum() / len(recommended_list)
 
     return precision
@@ -59,7 +68,6 @@ def recall_at_k(recommended_list, bought_list, k=5):
     recall = flags.sum() / len(bought_list)
 
     return recall
-
 
 def money_recall_at_k(recommended_list, bought_list, prices_recommended, prices_bought, k=5):
     # your_code
